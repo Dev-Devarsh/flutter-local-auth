cf8bcaa82fb3e17645a7ba93d59a313096713e0a
Devarsh-AD <devarsh@simpos.com.au>
2023-02-09 12:45:01 +0530

    added custom andriod settings

--- lib/service/local_auth_service.dart
+++ lib/service/local_auth_service.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 
 import 'package:local_auth/local_auth.dart';
-
+import 'package:local_auth_android/types/auth_messages_android.dart';
 
 class LocalAuth {
   static final _auth = LocalAuthentication();
@@ -15,9 +15,17 @@ class LocalAuth {
       log('a ${await availableBioMatrics.then((v) => v.map((e) => '${e} \n'))}');
       if (await _canAuthenticate()) {
         return await _auth.authenticate(
+            authMessages: [
+              AndroidAuthMessages(
+                  //! to change auth messages
+                  signInTitle: 'Auth required')
+            ],
             localizedReason: 'Please enter password',
             options: const AuthenticationOptions(
-                useErrorDialogs: true, stickyAuth: true,biometricOnly: true));
+              useErrorDialogs: true, stickyAuth: true,
+              //! if you enable [true] then you will not able use pattern lock only biomatric will be enabled
+              biometricOnly: false,
+            ));
       } else {
         return false;
       }
